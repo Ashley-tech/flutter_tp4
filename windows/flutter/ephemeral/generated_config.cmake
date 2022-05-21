713c48c13119656be144c94a799fb3c5795afeb1
Ashley-tech <58460632+Ashley-tech@users.noreply.github.com>
2022-05-21 23:43:50 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Ashley\\Downloads\\flutter_windows_2.10.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Ashley\\Downloads\\flutter_windows_2.10.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4"
+  "FLUTTER_ROOT=C:\\Users\\Ashley\\Downloads\\flutter_windows_2.10.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4"
+  "FLUTTER_TARGET=C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Ashley\\Documents\\PRISM\\Framework_de_developpement\\TP4\\tp4\\.dart_tool\\package_config.json"
+)
